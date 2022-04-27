6024249da7a14a38118dd72a786ce99203e55e00
Abel Abner <aang@Abels-MacBook-Pro.local>
2022-04-27 17:28:43 -0500

    exec-path-from-shell package installed and some required changes for make it work in the macbook

--- init.el
+++ init.el
@@ -1,7 +1,14 @@
 (require 'package)
 
-(add-to-list 'package-archives
+;; packages
+(when (>= emacs-major-version 24)
+  (package-initialize)
+  ;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+  )
+
+
 
 (setq package-user-dir
       (expand-file-name "packages" user-emacs-directory))
